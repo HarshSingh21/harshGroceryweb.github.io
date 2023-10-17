3607f6c6a336c4394b88ffbe702d1c9e9ac12f18
Harsh Singh <singhharsh8283@gmail.com>
2023-10-17 09:02:23 +0530

    Update sus.js

--- sus.js
+++ sus.js
@@ -1,5 +1,6 @@
 let searchForm = document.querySelector('.search-form');
 
+
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
     cart.classList.remove('active');
